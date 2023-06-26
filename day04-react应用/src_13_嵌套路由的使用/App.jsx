6e88b9718615b76a7ea3b0cbdd66b9897abddd06
hhy1999 <1992395918@qq.com>
2023-06-26 11:28:51 +0800

    feat: The use of router and parameter passing

---
+++ day04-react应用/src_13_嵌套路由的使用/App.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import MyNavLink from './component/MyNavLink'
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<div className="col-xs-offset-2 col-xs-8">
+						<div className="page-header"><h2>React Router Demo</h2></div>
+					</div>
+				</div>
+				{/* <BrowserRouter> 直接包裹app组件 */}
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+
+							{/* 原生html中，靠<a>跳转不同的页面 */}
+							{/* <a className="list-group-item" href="./about.html">About</a>
+							<a className="list-group-item active" href="./home.html">Home</a> */}
+
+							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
+							{/* <BrowserRouter> */}
+							{/* <NavLink activeClassName='active' className="list-group-item" to="/about">About</NavLink>
+								<NavLink activeClassName='active' className="list-group-item" to="/home">Home</NavLink> */}
+							{/* 标签体内容是一个特殊的属性 children属性 */}
+							{/**
+							|--------------------------------------------------
+							| 模糊匹配--尽量不使用严格匹配
+							|	1.默认使用的是模糊匹配（简单记：【输入的路径】必须包含要【匹配的路径】，且顺序要一致）
+							|	2.开启严格匹配：<Route exact={true} path="/about" component={About}/>
+							|	3.严格匹配不要随便开启，需要再开，有些时候开启会导致无法继续匹配二级路由
+							|--------------------------------------------------
+							*/}
+							<MyNavLink to="/about" title="About">About</MyNavLink>
+							<MyNavLink to="/home" title="Home">Home</MyNavLink>
+							{/* </BrowserRouter> */}
+						</div>
+					</div>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								{/* 注册路由 */}
+								{/* <BrowserRouter> 导致两个路由*/}
+								<Route path="/about" component={About} />
+								<Route path="/home" component={Home} />
+								{/* 兜底，重定向 */}
+								<Redirect to="/home"/>
+								{/* </BrowserRouter> */}
+							</div>
+						</div>
+					</div>
+				</div>
+				{/* </BrowserRouter> */}
+			</div>
+		)
+	}
+}
