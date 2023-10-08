68fddd4aadbf833413bd698fec59f47e4f181296
hhy1999 <1992395918@qq.com>
2023-10-08 16:31:37 +0800

    feat: test reduce

--- test/reduce/index.js
+++ test/reduce/index.js
@@ -4,5 +4,13 @@ const dataContent = {
     placeId: "em_p_001",
     ownOrgId: "org_001",
     factory: "郴州一厂",
-    
+    useOrgId: "org_002",
+    useEmployeeId: "em_002"
 }
+
+let obj = Object.keys(dataContent).reduce((acc, key, index, array) => {
+    if(dataContent[key] === 0){
+        delete dataContent[key]
+    }
+    return acc;
+}, {})
